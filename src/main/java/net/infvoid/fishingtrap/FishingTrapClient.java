daf7e390d6b738e6a0730acd22b3be3236d49964
Infv0id <abdullah.alrashoud@gmail.com>
2025-03-22 19:23:39 +0300

    Initial commit

---
+++ src/main/java/net/infvoid/fishingtrap/FishingTrapClient.java
@@ -0,0 +1,10 @@
+package net.infvoid.fishingtrap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingTrapClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/infvoid/fishingtrap/FishingTrapDataGenerator.java
@@ -0,0 +1,11 @@
+package net.infvoid.fishingtrap;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FishingTrapDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
