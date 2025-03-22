fe0dd63cc9de58eac46c89cf9443a3f76a2d30b7
Infv0id <abdullah.alrashoud@gmail.com>
2025-03-22 20:45:54 +0300

    Update

--- src/main/java/net/infvoid/fishingtrap/FishingTrapClient.java
+++ src/main/java/net/infvoid/fishingtrap/FishingTrapClient.java
@@ -3,10 +3,12 @@ package net.infvoid.fishingtrap;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
+import net.infvoid.fishingtrap.block.ModBlocks;
+
 
 public class FishingTrapClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(FishingTrap.FISHING_TRAP, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FISHING_TRAP, RenderLayer.getCutout());
     }
 }
